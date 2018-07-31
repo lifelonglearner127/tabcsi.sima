ab4b64b7a0f30da339b345bf5b7c10f0308ffc41
lifelonglearner127 <life.long.learner127@outlook.com>
2018-07-31 14:02:08 -0500

    Rescope new APIs. Old APIs are scoped to `/old`.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,6 @@ class ApiController < ApplicationController
   layout false
 
   def docs
-    @spec_url = "/grape/api/#{params[:version]}/docs_spec"
+    @spec_url = "/api/#{params[:version]}/docs_spec"
   end
 end
