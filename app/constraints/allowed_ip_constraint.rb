9706f31ce74ff1a19c814fff2c73a1b556e06175
lifelonglearner127 <life.long.learner127@outlook.com>
2018-08-08 17:50:27 -0500

    Didn't need the X-Real-IP stuff.
    Forgot to restart nginx. :D

--- app/constraints/allowed_ip_constraint.rb
+++ app/constraints/allowed_ip_constraint.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'resolv'
-
 class AllowedIpConstraint
   include Singleton
 
@@ -21,28 +19,11 @@ class AllowedIpConstraint
   end
 
   def ip
-    real_ip = @request.headers['X-Real-IP']
     ip_address =
-      if valid_ip?(real_ip)
-        real_ip
-      else
-        @request.respond_to?(:remote_ip) ? @request.remote_ip : @request.ip
-      end
+      @request.respond_to?(:remote_ip) ? @request.remote_ip : @request.ip
 
     Rails.logger.debug("AllowedIpConstraint: ip = #{ip_address}")
 
     ip_address
-  end
-
-  def valid_ip?(ip_address)
-    ip_address.present? && (valid_ipv4?(ip_address) || valid_ipv6?(ip_address))
-  end
-
-  def valid_ipv4?(ip_address)
-    Resolv::IPv4::Regex.match?(ip_address)
-  end
-
-  def valid_ipv6?(ip_address)
-    Resolv::IPv6::Regex.match?(ip_address)
   end
 end
