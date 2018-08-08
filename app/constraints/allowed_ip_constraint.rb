718398cf82432a52f0dc274782ed619dc229f077
lifelonglearner127 <life.long.learner127@outlook.com>
2018-08-08 16:38:12 -0500

    Use X-Real-IP request header if present and valid.

--- app/constraints/allowed_ip_constraint.rb
+++ app/constraints/allowed_ip_constraint.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'resolv'
+
 class AllowedIpConstraint
   include Singleton
 
@@ -19,11 +21,28 @@ class AllowedIpConstraint
   end
 
   def ip
+    real_ip = @request.headers['X-Real-IP']
     ip_address =
-      @request.respond_to?(:remote_ip) ? @request.remote_ip : @request.ip
+      if valid_ip?(real_ip)
+        real_ip
+      else
+        @request.respond_to?(:remote_ip) ? @request.remote_ip : @request.ip
+      end
 
     Rails.logger.debug("AllowedIpConstraint: ip = #{ip_address}")
 
     ip_address
+  end
+
+  def valid_ip?(ip_address)
+    ip_address.present? && (valid_ipv4?(ip_address) || valid_ipv6?(ip_address))
+  end
+
+  def valid_ipv4?(ip_address)
+    Resolv::IPv4::Regex.match?(ip_address)
+  end
+
+  def valid_ipv6?(ip_address)
+    Resolv::IPv6::Regex.match?(ip_address)
   end
 end
