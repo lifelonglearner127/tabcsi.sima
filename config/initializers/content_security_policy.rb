4c300200795e38dabab4829c98dfff6e6cbb2585
lifelonglearner127 <life.long.learner127@outlook.com>
2018-08-15 15:34:53 -0500

    Fix CSP errors in browsers when viewing API docs.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,11 +7,11 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https
+  policy.default_src :self, :https, :blob
   policy.font_src :self, :https, :data
   policy.img_src :self, :https, :data
   policy.object_src :none
-  policy.script_src :self, :https, :unsafe_eval, :unsafe_inline
+  policy.script_src :self, :https, :blob, :unsafe_eval, :unsafe_inline
   policy.style_src :self, :https, :unsafe_inline
 
   if Rails.env.development?
