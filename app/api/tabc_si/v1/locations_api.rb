437d472a2b37a782fba1b65a7fef152bc777cdc0
lifelonglearner127 <life.long.learner127@outlook.com>
2018-08-20 11:12:51 -0500

    TA-357

--- app/api/tabc_si/v1/locations_api.rb
+++ app/api/tabc_si/v1/locations_api.rb
@@ -43,7 +43,13 @@ module TabcSi
           get :history do
             location = params[:location]
 
-            respond location.inspections.includes(:audit_form, :user)
+            inspections =
+              location
+              .inspections
+              .includes(:audit_form, :user)
+              .order(updated_at: :desc)
+
+            respond inspections
           end
         end
       end
