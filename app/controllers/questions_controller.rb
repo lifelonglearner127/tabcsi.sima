4b98f583ed62627c057776505adfb177f208ab35
lifelonglearner127 <life.long.learner127@outlook.com>
2018-09-17 16:38:11 -0500

    TA-707

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,17 @@
 
 class QuestionsController < ApplicationController
   def help
-    @question_number = params.require(:number)
+    @question =
+      Question
+      .current_questions
+      .includes(:question_help_items)
+      .where(question_number: params.require(:number))
+      .first
   end
+
+  def parse_item(item)
+    Kramdown::Document.new(item.value).to_html
+  end
+
+  helper_method :parse_item
 end
