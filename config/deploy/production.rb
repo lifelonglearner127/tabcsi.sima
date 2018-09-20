8128cdca40c252e67be1196ff12b2d218ca8e17b
lifelonglearner127 <life.long.learner127@outlook.com>
2018-09-20 18:03:53 -0500

    Update production deployment config.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :deploy_to, "/srv/www/rails/#{fetch(:application)}"
 set :branch, 'master'
 
 server(
-  '?',
+  'ec2-52-61-205-129.us-gov-west-1.compute.amazonaws.com',
   user: 'tabcsi',
   roles: %w[app db web resque_worker resque_scheduler]
 )
