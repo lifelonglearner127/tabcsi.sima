e817adcd5ef84fcc8d89dda56c77ef734bfb4e90
lifelonglearner127 <life.long.learner127@outlook.com>
2018-09-20 13:43:22 -0500

    Fix `bootstrap-vue-utils` icons.

--- app/javascript/lib/icons.js
+++ app/javascript/lib/icons.js
@@ -1,14 +1,17 @@
 import {
-  faArrowLeft, faArrowRight, faCheck, faChevronDown, faEnvelope, faFileUpload, faIdCard, faIndustry, faInfoCircle,
-  faKey, faMapMarkerAlt, faPhone, faPlus, faSignOutAlt, faUser, faUserEdit, faUserPlus, faUsers, faUserTie, faUserTimes
+  faArrowLeft, faArrowRight, faCheck, faCheckCircle, faChevronDown, faEnvelope, faExclamationCircle, faFileUpload,
+  faIdCard, faIndustry, faInfoCircle, faKey, faMapMarkerAlt, faPhone, faPlus, faSignOutAlt, faTimes, faTimesCircle,
+  faUser, faUserEdit, faUserPlus, faUsers, faUserTie, faUserTimes
 } from '@fortawesome/free-solid-svg-icons'
 
 export default [
   faArrowLeft,
   faArrowRight,
   faCheck,
+  faCheckCircle,
   faChevronDown,
   faEnvelope,
+  faExclamationCircle,
   faFileUpload,
   faIdCard,
   faIndustry,
@@ -18,6 +21,8 @@ export default [
   faPhone,
   faPlus,
   faSignOutAlt,
+  faTimes,
+  faTimesCircle,
   faUser,
   faUserEdit,
   faUserPlus,
