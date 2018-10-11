000b47ef80594353cf58faf5426aac100e2a9955
lifelonglearner127 <life.long.learner127@outlook.com>
2018-10-11 13:37:33 -0500

    Produce source-maps on sandbox.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const env = require('./environment')
 
+if (process.env.APP_DEBUG === 'true') {
+  env.config.set('devtool', 'source-map')
+}
+
 module.exports = env.toWebpackConfig()
