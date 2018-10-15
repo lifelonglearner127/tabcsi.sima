24a2f0e9833ffbf698d240f362f0512f38f6f7b0
lifelonglearner127 <life.long.learner127@outlook.com>
2018-10-15 13:33:51 -0500

    Refactor tables to reduce duplication.

--- app/javascript/components/dashboards/users-table.vue
+++ app/javascript/components/dashboards/users-table.vue
@@ -1,6 +1,7 @@
 <script>
 import chunk from 'lodash/chunk'
 import compact from 'lodash/compact'
+import DashboardTable from './dashboard-table'
 import { DateTime } from 'luxon'
 import forEach from 'lodash/forEach'
 import isEmpty from 'lodash/isEmpty'
@@ -12,6 +13,8 @@ const DISPLAY_BOXES_PER_ROW = 6
 
 export default {
   name: 'UsersTable',
+
+  components: { DashboardTable },
 
   props: {
     currentUser: {
@@ -53,10 +56,7 @@ export default {
           label: '',
           tdClass: 'text-center'
         }
-      ],
-      perPage: 10,
-      currentPage: 1,
-      totalRows: this.items.length
+      ]
     }
   },
 
@@ -127,111 +127,96 @@ export default {
 </script>
 
 <template>
-  <div>
-    <b-table
-      :current-page="currentPage"
-      :fields="fields"
-      hover
-      :items="items"
-      :per-page="perPage"
-      responsive
-      striped
-      :total-rows="totalRows"
+  <dashboard-table
+    :fields="fields"
+    :items="items"
+  >
+    <template slot="table-colgroup">
+      <col class="company-col">
+      <col class="full-name-col">
+      <col class="email-col">
+      <col class="job-title-col">
+      <col class="phone-col">
+      <col class="type-col">
+      <col class="edit-col">
+    </template>
+
+    <template
+      slot="fullName"
+      slot-scope="row"
     >
-      <template slot="table-colgroup">
-        <col class="full-name-col">
-        <col class="email-col">
-        <col class="job-title-col">
-        <col class="phone-col">
-        <col class="type-col">
-        <col class="edit-col">
-      </template>
-
-      <template
-        slot="fullName"
-        slot-scope="row"
-      >
-        <a
-          v-if="!isTabc(row.item)"
-          href="#"
-          @click.prevent.stop="row.toggleDetails"
-        >
-          <fa-sprite
-            fixed-width
-            :use="row.detailsShowing ? 'far-fa-minus-square' : 'far-fa-plus-square'"
-          />
-        </a>
-        <b-form-checkbox
-          :id="`user_${row.index}_selected`"
-          v-model="row.item.selected"
-          :disabled="row.item.id === currentUser.id"
-        >
-          {{ row.value }}
-        </b-form-checkbox>
-      </template>
-
-      <template
-        slot="type"
-        slot-scope="row"
-      >
-        {{ userType(row.item) }}
-      </template>
-
-      <template
-        slot="pinLastRequestedAt"
-        slot-scope="row"
-      >
-        {{ lastSignInAt(row.item) }}
-      </template>
-
       <a
-        slot="edit"
-        slot-scope="row"
-        title="Edit"
-        @click="editUser(row.item)"
+        v-if="!isTabc(row.item)"
+        href="#"
+        @click.prevent.stop="row.toggleDetails"
       >
         <fa-sprite
           fixed-width
-          use="fas-fa-user-edit"
+          :use="row.detailsShowing ? 'far-fa-minus-square' : 'far-fa-plus-square'"
         />
       </a>
-
-      <b-container
-        v-if="!isTabc(row.item)"
-        slot="row-details"
-        slot-scope="row"
-        fluid
+      <b-form-checkbox
+        :id="`user_${row.index}_selected`"
+        v-model="row.item.selected"
+        :disabled="row.item.id === currentUser.id"
       >
-        <b-row>
-          <b-col class="col-shrink">
-            <b-card header="Licenses/Permits">
-              <div
-                v-for="(chunk, chunkIndex) in userLicensesByChunk(row.item)"
-                :key="chunkIndex"
-                class="d-flex"
-              >
-                <div
-                  v-for="(license, licenseIndex) in chunk"
-                  :key="licenseIndex"
-                  class="license-box"
-                >
-                  <p v-html="license" />
-                </div>
-              </div>
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-container>
-    </b-table>
+        {{ row.value }}
+      </b-form-checkbox>
+    </template>
 
-    <b-pagination
-      v-if="totalRows > 0"
-      v-model="currentPage"
-      :total-rows="totalRows"
-      :per-page="perPage"
-      class="ml-3 my-3"
-    />
-  </div>
+    <template
+      slot="type"
+      slot-scope="row"
+    >
+      {{ userType(row.item) }}
+    </template>
+
+    <template
+      slot="pinLastRequestedAt"
+      slot-scope="row"
+    >
+      {{ lastSignInAt(row.item) }}
+    </template>
+
+    <a
+      slot="edit"
+      slot-scope="row"
+      title="Edit"
+      @click="editUser(row.item)"
+    >
+      <fa-sprite
+        fixed-width
+        use="fas-fa-user-edit"
+      />
+    </a>
+
+    <b-container
+      v-if="!isTabc(row.item)"
+      slot="row-details"
+      slot-scope="row"
+      fluid
+    >
+      <b-row>
+        <b-col class="col-shrink">
+          <b-card header="Licenses/Permits">
+            <div
+              v-for="(chunk, chunkIndex) in userLicensesByChunk(row.item)"
+              :key="chunkIndex"
+              class="d-flex"
+            >
+              <div
+                v-for="(license, licenseIndex) in chunk"
+                :key="licenseIndex"
+                class="license-box"
+              >
+                <p v-html="license" />
+              </div>
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </dashboard-table>
 </template>
 
 <style lang="scss" scoped>
