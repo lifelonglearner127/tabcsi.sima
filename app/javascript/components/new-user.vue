9310fba50ebe414b57e28c037bbce0660c28b83d
lifelonglearner127 <life.long.learner127@outlook.com>
2018-10-16 05:56:19 +1000

    TA-870 -- make location box on invite page as well

--- app/javascript/components/new-user.vue
+++ app/javascript/components/new-user.vue
@@ -1,6 +1,7 @@
 <script>
 import { email, fullName, licenseNumber, phone } from '~/validators'
 import { AsYouType } from '~/lib/phone-number'
+import compact from 'lodash/compact'
 import map from 'lodash/map'
 import { parseDigit } from '~/lib/utils'
 import { required } from 'vuelidate/lib/validators'
@@ -148,7 +149,7 @@ export default {
       return map(
         this.locations,
         (location) => ({
-          text: `${location.name} (${location.clp})`,
+          text: compact([location.name, this.locationAddress(location), location.clp]).join('<br>'),
           value: location.clp
         })
       )
@@ -169,6 +170,21 @@ export default {
   },
 
   methods: {
+    locationAddress (location) {
+      return compact([
+        location.address1,
+        location.address2,
+        location.address3,
+        compact([
+          location.city,
+          location.county,
+          location.state,
+          location.postalCode
+        ]).join(' '),
+        location.country
+      ]).join(', ')
+    },
+
     formatPhone (value) {
       const asYouType = new AsYouType()
       const text = asYouType.input(value)
