bb574b68e90b9e5c1d2c89fad1d79a61034ad774
lifelonglearner127 <life.long.learner127@outlook.com>
2018-10-17 04:42:02 +1000

    TA-949

--- app/javascript/components/dashboards/locations-table.vue
+++ app/javascript/components/dashboards/locations-table.vue
@@ -1,6 +1,9 @@
 <script>
 import compact from 'lodash/compact'
 import DashboardTable from './dashboard-table'
+import forEach from 'lodash/forEach'
+import includes from 'lodash/includes'
+import isEmpty from 'lodash/isEmpty'
 import map from 'lodash/map'
 
 export default {
@@ -30,7 +33,20 @@ export default {
           tdClass: 'text-center',
           thClass: 'text-center'
         }
-      ]
+      ],
+      filteredItems: this.items,
+      searchOptions: [
+        {
+          text: 'Name (Company name)',
+          value: 'name'
+        },
+        {
+          text: 'CLP Number',
+          value: 'clp'
+        }
+      ],
+      searchOption: 'name',
+      searchKey: null
     }
   },
 
@@ -67,43 +83,90 @@ export default {
       }
 
       return ''
+    },
+
+    filterLocations () {
+      this.filteredItems = []
+      forEach(
+        this.items,
+        (location) => {
+          if (isEmpty(this.searchKey)) {
+            this.filteredItems.push(location)
+
+            return
+          }
+          if (this.searchOption === 'name') {
+            if (includes(location.name.toLowerCase(), this.searchKey.toLowerCase())) {
+              this.filteredItems.push(location)
+            }
+          } else if (this.searchOption === 'clp') {
+            if (location.clp.toLowerCase() === this.searchKey.toLowerCase()) {
+              this.filteredItems.push(location)
+            }
+          }
+        }
+      )
     }
   }
 }
 </script>
 
 <template>
-  <dashboard-table
-    :fields="fields"
-    :items="items"
-  >
-    <template slot="table-colgroup">
-      <col class="name-col">
-      <col class="clp-col">
-      <col class="address-col">
-      <col class="phone-col">
-      <col class="status-col">
-    </template>
-
-    <div
-      slot="clp"
-      slot-scope="row"
-      v-html="licenses(row.item)"
-    />
-
-    <address
-      slot="address"
-      slot-scope="row"
-      v-html="address(row.item)"
-    />
-
-    <h6
-      slot="status"
-      slot-scope="row"
+  <div>
+    <b-button-toolbar
+      class="mb-3 ml-3"
     >
-      {{ status(row.item) }}
-    </h6>
-  </dashboard-table>
+      <b-form-input
+        v-model="searchKey"
+        class="w-25 mx-1"
+      />
+
+      <b-form-select
+        v-model="searchOption"
+        :options="searchOptions"
+        class="w-25 mx-1"
+      />
+
+      <b-btn
+        variant="outline-secondary"
+        @click="filterLocations"
+      >
+        Search
+      </b-btn>
+    </b-button-toolbar>
+
+    <dashboard-table
+      :fields="fields"
+      :items="filteredItems"
+    >
+      <template slot="table-colgroup">
+        <col class="name-col">
+        <col class="clp-col">
+        <col class="address-col">
+        <col class="phone-col">
+        <col class="status-col">
+      </template>
+
+      <div
+        slot="clp"
+        slot-scope="row"
+        v-html="licenses(row.item)"
+      />
+
+      <address
+        slot="address"
+        slot-scope="row"
+        v-html="address(row.item)"
+      />
+
+      <h6
+        slot="status"
+        slot-scope="row"
+      >
+        {{ status(row.item) }}
+      </h6>
+    </dashboard-table>
+  </div>
 </template>
 
 <style lang="scss" scoped>
