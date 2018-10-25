53119a88e1da3e95a0239ad39b99ddfbe77c07ab
lifelonglearner127 <life.long.learner127@outlook.com>
2018-10-25 13:33:12 -0500

    TA-1019

--- app/javascript/packs/images.js
+++ app/javascript/packs/images.js
@@ -1,4 +1,5 @@
 import '~/assets/images/help/consumption_warning_sign.png'
 import '~/assets/images/help/handgun_warning_sign-blue-english.png'
 import '~/assets/images/help/handgun_warning_sign-red-english.png'
+import '~/assets/images/help/health_risk_warning_sign.png'
 import '~/assets/images/help/public_information_complaint_sign.png'
