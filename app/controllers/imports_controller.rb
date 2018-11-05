9db042f905c890d38e3b77e9a4e75c59e1388f3f
lifelonglearner127 <life.long.learner127@outlook.com>
2018-11-05 10:27:27 -0600

    Update dependencies.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 class ImportController < ApplicationController
   before_action :require_logged_in_user
   before_action :set_page_options, only: %i[upload_csv]
@@ -9,6 +11,7 @@ class ImportController < ApplicationController
   def users
     upload_file = import_params[:file]
     csv_file = move_file(upload_file)
+
     CsvJob.perform_later(csv_file, current_user)
 
     redirect_to dashboard_url
@@ -17,12 +20,9 @@ class ImportController < ApplicationController
   private
 
   def move_file(file)
-    require 'fileutils'
+    target = "tmp/uploads/#{Time.now.to_i}_#{file.original_filename}"
 
-    target = File.join(
-      'tmp', 'uploads', "#{Time.now.to_i}_#{file.original_filename}"
-    )
-    FileUtils.cp file.path, target
+    FileUtils.cp(file.path, target)
 
     target
   end
@@ -31,21 +31,11 @@ class ImportController < ApplicationController
     params.require(:import).permit(:file)
   end
 
-  def build_page_options(page_name)
-    reset_page_options(controller_page_options(page_name))
-  end
-
-  def controller_page_options(page_name = action_name)
-    case page_name
-    when 'upload_csv'
-      {
-        url: import_users_path,
-        method: 'post',
-        local: true,
-        html: {}
-      }
-    else
-      {}
-    end
+  def controller_page_options
+    {
+      url: import_users_path,
+      method: 'post',
+      local: true
+    }
   end
 end
